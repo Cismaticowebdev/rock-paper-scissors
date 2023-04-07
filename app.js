3a56593943c6e3448e325ee07c3eed2736adbf7d
cismaticowebdev <cismaticowebdev@gmail.com>
2023-04-07 17:39:28 +0100

    Add 'game()' function to start the game Add 'getPlayerChoice()' function to get the player choice using a prompt Add 'console.log()' to every return in 'playRound()'

--- app.js
+++ app.js
@@ -9,18 +9,32 @@ function getComputerChoice() {
 function playRound(playerChoice, computerChoice) {
 
   if (playerChoice === 'rock' && computerChoice === 'paper') {
-    return `You lose! ${computerChoice} beats ${playerChoice}`;
+    return console.log(`You lose! ${computerChoice} beats ${playerChoice}`);
   } else if (playerChoice === 'rock' && computerChoice === 'scissors') {
-    return `You win! ${playerChoice} beats ${computerChoice}`;
+    return console.log(`You win! ${playerChoice} beats ${computerChoice}`);
   } else if (playerChoice === 'paper' && computerChoice === 'rock') {
-    return `You win! ${playerChoice} beats ${computerChoice}`;
+    return console.log(`You win! ${playerChoice} beats ${computerChoice}`);
   } else if (playerChoice === 'paper' && computerChoice === 'scissors') {
-    return `You lose! ${computerChoice} beats ${playerChoice}`;
+    return console.log(`You lose! ${computerChoice} beats ${playerChoice}`);
   } else if (playerChoice === 'scissors' && computerChoice === 'rock') {
-    return `You lose! ${computerChoice} beats ${playerChoice}`;
+    return console.log(`You lose! ${computerChoice} beats ${playerChoice}`);
   } else if (playerChoice === 'scissors' && computerChoice === 'paper') {
-    return `You win! ${playerChoice} beats ${computerChoice}`;
+    return console.log(`You win! ${playerChoice} beats ${computerChoice}`);
   } else {
-    return 'It is a draw';
+    return console.log('It is a draw');
   }
+}
+
+function game() {
+  let gameLength = 5;
+
+  for (let i = 0; i < gameLength; i++) {
+    playRound(getPlayerChoice(), getComputerChoice());
+  }
+}
+
+function getPlayerChoice() {
+  let playerChoice = String(prompt('Write your choice "rock", "paper" or "scissors"'));
+
+  return playerChoice.toLowerCase();
 }
