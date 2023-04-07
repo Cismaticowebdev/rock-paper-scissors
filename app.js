037441f5c997eb4a2725d334e478c7a1bdfa82e1
cismaticowebdev <cismaticowebdev@gmail.com>
2023-04-07 17:21:53 +0100

    Create 'playRound()' function, it takes 2 parameters (the player and computer choices) to decide who is the winner of the round

--- app.js
+++ app.js
@@ -1,7 +1,26 @@
 
 function getComputerChoice() {
-  let computerChoiceArray = ['Rock', 'Paper', 'Scissors'];
-  let randomNumber = Math.floor(Math.random()*computerChoiceArray.length);
+  let computerChoiceArray = ['rock', 'paper', 'scissors'];
+  let randomNumber = Math.floor(Math.random() * computerChoiceArray.length);
 
   return computerChoiceArray[randomNumber];
+}
+
+function playRound(playerChoice, computerChoice) {
+
+  if (playerChoice === 'rock' && computerChoice === 'paper') {
+    return `You lose! ${computerChoice} beats ${playerChoice}`;
+  } else if (playerChoice === 'rock' && computerChoice === 'scissors') {
+    return `You win! ${playerChoice} beats ${computerChoice}`;
+  } else if (playerChoice === 'paper' && computerChoice === 'rock') {
+    return `You win! ${playerChoice} beats ${computerChoice}`;
+  } else if (playerChoice === 'paper' && computerChoice === 'scissors') {
+    return `You lose! ${computerChoice} beats ${playerChoice}`;
+  } else if (playerChoice === 'scissors' && computerChoice === 'rock') {
+    return `You lose! ${computerChoice} beats ${playerChoice}`;
+  } else if (playerChoice === 'scissors' && computerChoice === 'paper') {
+    return `You win! ${playerChoice} beats ${computerChoice}`;
+  } else {
+    return 'It is a draw';
+  }
 }
